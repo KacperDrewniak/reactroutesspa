31f714164e051650b8d271ca98b4f8c732fd814e
KacperDrewniak <50110237+KacperDrewniak@users.noreply.github.com>
2019-11-01 05:35:45 +0100

    Add routes

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,7 +1,15 @@
 import React from "react";
+import "../styles/header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  const img1 = "https://cdn.pixabay.com/photo/2017/08/06/18/05/pumpkin-2594747__340.jpg";
+  const img2 = "https://cdn.pixabay.com/photo/2016/01/22/23/06/flash-1156822__340.jpg";
+  const img3 = "https://cdn.pixabay.com/photo/2017/10/31/15/16/halloween-2905531__340.jpg";
+  return (
+    <div>
+      <img src={img1} alt="" />
+    </div>
+  );
 };
 
 export default Header;
--- src/layouts/Navigation.js
+++ src/layouts/Navigation.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import "../styles/navigation.css";
+const list = [
+  { name: "Start", path: "/", exact: true },
+  { name: "Produkty", path: "/products" },
+  { name: "Kontakt", path: "/contact" },
+  { name: "Panel", path: "/admin" }
+];
 
 const Navigation = () => {
-  return <div>Nav</div>;
+  const menu = list.map(item => (
+    <li key={item.name}>
+      <NavLink exact={item.exact ? item.exact : false} to={item.path}>
+        {item.name}
+      </NavLink>
+    </li>
+  ));
+  return (
+    <nav>
+      <ul>{menu}</ul>
+    </nav>
+  );
 };
 
 export default Navigation;
