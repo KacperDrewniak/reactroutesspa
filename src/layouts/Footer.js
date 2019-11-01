1b58e069e20459b5187e4a39c7ea34b57d26ff52
KacperDrewniak <50110237+KacperDrewniak@users.noreply.github.com>
2019-11-01 18:53:50 +0100

    F**ing footer

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,7 +1,19 @@
 import React from "react";
-
+import { Route } from "react-router-dom";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <>
+      <h2>Stopka</h2>
+      <Route
+        path="/"
+        exact
+        render={props => {
+          console.log(props);
+          return <p>Dodatkowe informacje</p>;
+        }}
+      />
+    </>
+  );
 };
 
 export default Footer;
